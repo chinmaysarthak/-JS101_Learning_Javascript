5220eea9d880b4a39979e895fc5543d838f6fbd4
chinmay-sarthak <chinmaysarthak@gmail.com>
2022-10-16 23:37:57 +0530

    logical and ternary operator added

---
+++ day2/p4.js
@@ -0,0 +1,9 @@
+//Problem 4: Given 3 numbers (all different values), print which is greatest
+
+let a= 20;
+let b= 30;
+let c= 40;
+
+a>b&&a>c ? console.log(a+" "+"is greater.")
+  :b>a&&b>c ? console.log(b+" "+"is greater.")
+  :console.log(c+" "+"is greater.")
